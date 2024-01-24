55c8297181112af6770da6dc6af40a7ba52cd344
playerRC <rachid.estrada@gmail.com>
2024-01-24 17:50:38 +0100

    Add elastic cloud client

--- config.py
+++ config.py
@@ -1,5 +1,15 @@
 from flask import Blueprint, request, jsonify, Response
 from elasticsearch import Elasticsearch
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+ELASTIC_PASSWORD = os.getenv("ELASTIC_PASSWORD")
+CLOUD_ID = os.getenv("CLOUD_ID")
 
 # Initialisation de l'instance Elasticsearch
-client = Elasticsearch("http://localhost:9200")
+client = Elasticsearch(
+    cloud_id=CLOUD_ID,
+    basic_auth=("elastic", ELASTIC_PASSWORD)
+)
