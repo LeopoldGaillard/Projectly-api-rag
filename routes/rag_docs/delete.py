bcfafefba9257e6129f6012fed15cc25633d7dd6
playerRC <rachid.estrada@gmail.com>
2024-02-01 10:15:43 +0100

    Update delete doc by id route

--- routes/rag_docs/delete.py
+++ routes/rag_docs/delete.py
@@ -5,5 +5,8 @@ delete_doc = Blueprint('delete_doc', __name__)
 # Route qui permet de supprimer un document
 @delete_doc.route('/rag_docs/delete/<id>', methods=['DELETE'])
 def delete_file(id):
-    response = client.delete(index="projectly", id=id)
+    response = client.delete_by_query(
+        index="projectly",
+        body={"query": {"match": {"id": id}}},
+    )
     return jsonify(response.body)
